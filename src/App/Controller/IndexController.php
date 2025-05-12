25415ff9704a1528897745113a0a9b8330e67cd9
Mike Sweeney <blindx@gmail.com>
2025-05-12 01:38:09 -0400

    Compile stylesheets server-side

--- src/App/Controller/IndexController.php
+++ src/App/Controller/IndexController.php
@@ -3,12 +3,21 @@
 namespace App\Controller;
 
 use PHPSkel\Controller;
-use PHPSkel\HttpException;
+use PHPSkel\Response;
 
 class IndexController extends Controller
 {
     public function indexAction()
     {
         return $this->render('index.tpl', ['title' => 'Home']);
+    }
+
+    public function styleAction()
+    {
+        $css = file_get_contents(ROOTDIR . '/web/css/colors.css');
+        $css .= file_get_contents(ROOTDIR . '/web/css/base.css');
+        $css .= file_get_contents(ROOTDIR . '/web/css/utility.css');
+
+        return new Response($css, 200, ['Content-Type' => 'text/css']);
     }
 }
