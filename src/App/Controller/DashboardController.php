2aee1fc2d3f2cd3f9897de51f0c754022272d4dc
Mike Sweeney <blindx@gmail.com>
2025-05-13 01:39:55 -0400

    Sort roles by their position on the server

--- src/App/Controller/DashboardController.php
+++ src/App/Controller/DashboardController.php
@@ -62,11 +62,23 @@ class DashboardController extends Controller
 
     public function serverDashboardAction($server)
     {
+        $roles = $this->jarvisRoleDatabase->getRoles($server);
+        // sort by role.position, backwards
+        usort($roles, function($a, $b) {
+            if ($a->position < $b->position) {
+                return 1;
+            } elseif ($a->position > $b->position) {
+                return -1;
+            }
+
+            return 0;
+        });
+
         $pageData = [
             'title'    => 'Dashboard',
             'server'   => $this->jarvisServerDatabase->getServer($server),
             'channels' => $this->jarvisChannelDatabase->getChannels($server),
-            'roles'    => $this->jarvisRoleDatabase->getRoles($server),
+            'roles'    => $roles,
             'configs'  => $this->configDatabase->getConfigs($server),
         ];
 
